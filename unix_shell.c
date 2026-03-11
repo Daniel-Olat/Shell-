c8e93b94f0e41027583aa4c230f0a8386d4c4015
Daniel Olatunji <danielolatunji2907@gmail.com>
2026-03-11 18:51:50 +0100

    UNIX SHELL

---
+++ unix_shell.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc , char **argv){
+    // Load config files if any
+    // Basic command loop
+    lsh_loop();
+    return EXIT_SUCCESS;//any shutdown or cleaning
+}
